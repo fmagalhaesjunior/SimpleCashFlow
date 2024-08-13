c2b57cb6ecd43b30641838fb99ab1880d0a2023d
Francisco Magalhaes <francisco.magalhaes@fmbtecnologia.com.br>
2024-08-13 11:31:02 -0300

    Ajuste ao deletar transação

--- TransactionService/Application/Commands/DeleteTransaction.cs
+++ TransactionService/Application/Commands/DeleteTransaction.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Interfaces;
 using MediatR;
-using System.Transactions;
+using Core.Entities;
+using Newtonsoft.Json;
 
 namespace Application.Commands
 {
@@ -9,17 +10,26 @@ namespace Application.Commands
     public class DeleteTransactionCommandHandler : IRequestHandler<DeleteTransactionCommand>
     {
         private readonly IService<Transaction> _service;
-        public DeleteTransactionCommandHandler(IService<Transaction> service)
+        private readonly IMessageQueueService _messageQueueService;
+        public DeleteTransactionCommandHandler(IService<Transaction> service, IMessageQueueService messageQueueService)
         {
             _service = service;
+            _messageQueueService = messageQueueService;
         }
         public Task Handle(DeleteTransactionCommand request, CancellationToken cancellationToken)
         {
             var transactionDb = _service.GetById<Transaction>(request.id);
             if (transactionDb == null)
                 throw new ArgumentNullException("Transação não encontrada.");
-
+            
+            var deleteDailySummary = new
+            {
+                Amount = transactionDb.Amount,
+                Date = transactionDb.TransactionDate
+            };
             _service.Remove(transactionDb);
+            var message = JsonConvert.SerializeObject(deleteDailySummary);
+            _messageQueueService.SendMessage("delete_summary", message);
             return Task.CompletedTask;
         }
     }
